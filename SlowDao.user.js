a8587859cfedd2583e0eb5d56b42e032e19a83ff
shhysy <150139065+shhysy@users.noreply.github.com>
2025-05-08 00:51:34 -0700

    Add files via upload

---
+++ SlowDao.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         SlowDao
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Auto-updating userscript for SlowDao
+// @author       Your name
+// @match        *://*/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/shhysy/test/main/SlowDao.js
+// @downloadURL  https://raw.githubusercontent.com/shhysy/test/main/SlowDao.js
+// @supportURL   https://github.com/shhysy/test/issues
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // Your existing code will go here
+    // Please paste your SlowDao.js code below this line
+    
+})(); 
