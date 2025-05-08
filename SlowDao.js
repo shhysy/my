b9f31a00e2851916a96a059d31a7671d6527731b
shhysy <shhysy@example.com>
2025-05-08 01:23:15 -0700

    auto: 升级 SlowDao.js 到 v1.5

--- SlowDao.js
+++ SlowDao.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SlowDao
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  Auto-updating userscript for SlowDao
 // @author       Your name
 // @match        *://*/*
