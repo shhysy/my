4fa0c8a97370797a936b0e7ca87acc4e31bfdb60
shhysy <150139065+shhysy@users.noreply.github.com>
2025-05-08 00:32:55 -0700

    SlowDao.meta.js

---
+++ SlowDao.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         SlowDao
+// @namespace    http://tampermonkey.net/
+// @version      1.9
+// @description  开启web3之旅
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
