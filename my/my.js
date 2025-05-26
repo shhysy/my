e249f3c3cb7c49f1570e8bde4612dc41b714350e
shhysy <2157936243@qq.com>
2025-05-26 13:47:38 +0800

    auto: 升级 SlowDao.js 到 v47.133

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.132
+// @version      47.133
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -7317,7 +7317,7 @@
             }
         });
     
-        if (matchCount === 2) { // Exactly 3 matches
+        if (matchCount > 2) { // Exactly 3 matches
             setTimeout(() => {
                 location.href = 'https://chat.chainopera.ai/login';
             }, 15000);
