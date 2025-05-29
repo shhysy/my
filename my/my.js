7f5b403822ced30af0ec47f15ad51d859d4dba36
shhysy <2157936243@qq.com>
2025-05-29 18:37:20 +0800

    auto: 升级 SlowDao.js 到 v47.159

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.158
+// @version      47.159
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -5061,7 +5061,7 @@
 
     // 生成随机数字（0.0111到0.02，4位小数）
     function getRandomAmount() {
-        const min = 0.0000111;
+        const min = 0.00001;
         const max = 0.00002;
         const amount = (Math.random() * (max - min) + min).toFixed(4);
         log(`生成随机金额: ${amount}`);
