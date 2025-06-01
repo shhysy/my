35c6dac0b5b8519f8e9ae0b4ec0482289d9db9e7
shhysy <2157936243@qq.com>
2025-06-01 21:56:57 +0800

    auto: 升级 SlowDao.js 到 v47.194

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.193
+// @version      47.194
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -1013,7 +1013,7 @@
                 buttons.forEach(button => {
                     if (button.textContent.trim().includes('Claimed')) {
                         button.click();
-                        window.location.href = 'http://www.360.com';
+                        window.location.href = 'https://app.yala.org/berries';
                         clearInterval(Claimed);
                     }
                 });
