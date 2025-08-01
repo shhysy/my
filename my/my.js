538d3f5fa16d50d9fbfbc0e402e9950783ea832c
shhysy <2157936243@qq.com>
2025-08-01 21:54:56 +0800

    auto: 升级 SlowDao.js 到 v47.328

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.327
+// @version      47.328
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -8197,16 +8197,16 @@
     }, 5000);
 
 
-    const USDT = setInterval(() => {
-        const buttons = document.querySelectorAll('button');
-        buttons.forEach(button => {
-            if (button.textContent.trim().includes('USDT') &&
-                !button.hasAttribute('disabled')) {
-                button.click();
-                clearInterval(USDT);
-            }
-        });
-    }, 5000);
+    // const USDT = setInterval(() => {
+    //     const buttons = document.querySelectorAll('button');
+    //     buttons.forEach(button => {
+    //         if (button.textContent.trim().includes('USDT') &&
+    //             !button.hasAttribute('disabled')) {
+    //             button.click();
+    //             clearInterval(USDT);
+    //         }
+    //     });
+    // }, 5000);
 
     // const WBTC = setInterval(() => {
     //     const buttons = document.querySelectorAll('button');
