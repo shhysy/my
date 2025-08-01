0d92ac2f2fd162191d5e0820e461bdf498b637f2
shhysy <2157936243@qq.com>
2025-08-01 18:58:06 +0800

    auto: 升级 SlowDao.js 到 v47.295

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.294
+// @version      47.295
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -8205,7 +8205,7 @@
             if (button.textContent.trim().includes('Review Swap') &&
                 !button.hasAttribute('disabled')) {
                 button.click();
-                //clearInterval(ReviewSwap);
+                clearInterval(ReviewSwap);
             }
         });
     }, 5000);
