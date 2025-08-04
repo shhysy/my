096b6eecddf11eb8b0c30b80a899b67500318152
shhysy <2157936243@qq.com>
2025-08-04 16:45:22 +0800

    auto: 升级 SlowDao.js 到 v47.367

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.363
+// @version      47.367
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -24,6 +24,9 @@
     setInterval(() => {
         // 检查当前域名是否为 testnet.pharosscan.xyz 且 URL 包含 /tx
         if (location.hostname === 'testnet.pharosscan.xyz' && location.pathname.startsWith('/tx')) {
+            window.close();
+        }
+        if (location.hostname=='pay.primuslabs.xyz') {
             window.close();
         }
     }, 1000);
