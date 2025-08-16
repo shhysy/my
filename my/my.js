ae89cb2cab35260e6dc7e9ed0309ede9a440dfa3
shhysy <2157936243@qq.com>
2025-08-16 22:20:43 +0800

    auto: 升级 my.js 到 v47.401

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.400
+// @version      47.401
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -4529,7 +4529,7 @@
             const conversationSuccess = await performConversations();
 
             if (conversationSuccess) {
-                window.location.href = 'https://testnet.somnia.network/';
+                window.location.href = 'https://dashboard.monadscore.xyz/dashboard';
                 console.log('所有对话完成');
             } else {
                 performConversations();
