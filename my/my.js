f566ddccdfe4e11ef055ecd229dad3376df62faf
shhysy <2157936243@qq.com>
2025-08-19 23:29:24 +0800

    auto: 升级 my.js 到 v47.426

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      47.425
+// @version      47.426
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -4588,7 +4588,7 @@
 
     setTimeout(() => {
         main();
-    }, 5000);
+    }, 10000);
 })();
 
 
