5983ffad99274d8310c85e00c31860569f87fa70
shhysy <2157936243@qq.com>
2025-09-09 13:51:35 +0800

    auto: 升级 my.js 到 v48.4

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      48.2
+// @version      48.4
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -8817,7 +8817,7 @@
         if (document.readyState === 'complete') {
             const buttons = document.querySelectorAll('button');
             buttons.forEach(button => {
-                if (button.textContent.trim().includes('OKX Wallet') &&
+                if (button.textContent.trim().includes('MetaMask') &&
                     !button.hasAttribute('disabled')) {
                     button.click();
                     clearInterval(OKXWallet);
