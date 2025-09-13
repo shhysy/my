7b11d703ed9291548f46576b7b10cf38f6994739
shhysy <2157936243@qq.com>
2025-09-13 23:05:28 +0800

    auto: 升级 my.js 到 v48.8

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      48.7
+// @version      48.8
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -1391,11 +1391,11 @@
         return;
     }
 
-    // setInterval(() => {
-    //     if (window.location.href !== 'https://sosovalue.com/assets/crypto-stocks?action=share&tid=soso-airdrop-exp-daily_task') {
-    //         window.location.href = 'https://sosovalue.com/exp';
-    //     }
-    // }, 15000);
+    setInterval(() => {
+        if (window.location.href == 'https://sosovalue.com/assets/crypto-stocks?action=share&tid=soso-airdrop-exp-daily_task') {
+            window.location.href = 'https://sosovalue.com/exp';
+        }
+    }, 15000);
 
     var checkP = true;
     var f =1
@@ -1489,7 +1489,7 @@
                 console.log("Buttons found, attempting to click...");
                 for (let i = 0; i < buttons.length; i++) {
                     if (!buttons[i].disabled) {
-                        if(i!=0){
+                        if(i!=1){
                             buttons[i].click();
                             allDisabled = 0; // Reset
                         }
