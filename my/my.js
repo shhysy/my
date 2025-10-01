0cab7f386112316d93907cd28d71c02302c63b70
shhysy <2157936243@qq.com>
2025-10-01 20:05:56 +0800

    auto: 升级 my.js 到 v48.23

--- my/my.js
+++ my/my.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DAO
 // @namespace    http://tampermonkey.net/
-// @version      48.22
+// @version      48.23
 // @description  空投
 // @author       开启数字空投财富的发掘之旅
 // @match        *://*/*
@@ -1663,7 +1663,7 @@
     setInterval(() => {
         clickButtons();
         if (allDisabled>=5) {
-            window.location.href = 'https://x.ink/airdrop';
+            window.location.href = 'https://cryptopond.xyz/modelfactory/detail/306250?tab=4';
         }
     }, 3000);
 
